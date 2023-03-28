f5e95241af35394ddd2290a6d59f7593b69b2231
Very1Fake <Very1Fake@users.noreply.github.com>
2023-03-28 18:53:32 +0000

    Deploying to gh-pages from @ Very1Fake/solid-demo@b712bad18b6b75460daed4c3bd0b2bdeef6c1205 🚀

---
+++ assets/index-1cef8aa8.js
@@ -0,0 +1,1 @@
+import{g as e,t}from"./entry-client-1b1a6ea9.js";const a=t("<a>Profile Page</a>",2);function r(){return e(a)}export{r as default};
--- assets/index-4219f27f.js
+++
@@ -1,1 +0,0 @@
-import{g as e,t}from"./entry-client-52d8506d.js";const a=t("<a>Profile Page</a>",2);function r(){return e(a)}export{r as default};
